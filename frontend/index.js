02230a474c72db15c6c4ce9a01241bff0367b335
sriramongit <sriramsahu187@gmail.com>
2025-06-28 16:33:55 +0530

    Creating the add candidate page

--- frontend/index.js
+++ frontend/index.js
@@ -1,35 +1,61 @@
-import { courses } from "../data/courses.js";
-import { seatMatrix } from "../seatMatrix.js";
-import { candidates } from "../data/candidates.js";
+
+const name = document.querySelector('.input-name');
+const rollNo = document.querySelector('.input-rollno');
+const rank = document.querySelector('.input-rank');
+const choice = document.querySelector('.input-choice');
+
+let branches = [];
+
+const addCadidate_btn = document.querySelector('.btn-addCandidate');
+const removeCandidate = document.querySelector('.btn-removeCandidate')
+
+let preview_name = document.querySelector('.credential-name');
+let preview_rollno = document.querySelector(".credential-rollno");
+let preview_rank = document.querySelector(".credential-rank");
+let preview_branches = document.querySelector(".credential-branches");
+let addChoice = document.querySelector(".btn-add-choice")
 
 
-//sorting the students rankwise
-let rankwise = candidates.sort((a, b) => a.rank - b.rank);
+function validateError(error) { 
+  if (error === 'empty field error') { 
+    setTimeout(() => {
+      document.querySelector(".error").innerHTML = "";
+    }, 1000);
 
-for (let i = 0; i < rankwise.length; i++) {
-  let candidate = rankwise[i]; 
-  let choice = Object.values(candidate.choices);
-
-  innerloop:
-  //iterating through each choice 
-  for (let j = 0; j < choice.length; j++) {
-    //iterating through each course that the college provides 
-    for (let k = 0; k < courses.length; k++) {
-      //check for seats
-      if (choice[j] === courses[k].name && courses[k].seats !== 0) {
-        // Optionally assign the course to the candidate here
-        seatMatrix.push({
-          name: candidate.name,
-          candidate_rank: candidate.rank,
-          alloted_branch: courses[k].name
-        });
-
-        courses[k].seats -= 1; //decrement the available seats
-        break innerloop;
-      }
-    }
+    document.querySelector(".error").innerHTML = "Enter all fields first";
   }
 }
 
-console.log(seatMatrix);
+
+
+addCadidate_btn.addEventListener('click', () => { 
+
+  //validating error if fields are empty
+  if (name.value === '' || rollNo.value === '' || rank.value === '' || choice.value === '') { 
+    const error = 'empty field error';
+    validateError(error);
+  }
+
+  preview_name.innerHTML = `Name: ${name.value.toUpperCase()}`
+  preview_rollno.innerHTML = `Roll No: ${rollNo.value}`
+  preview_rank.innerHTML = `Rank: ${rank.value}`
+  preview_branches.innerHTML = `Choices: ${choice.value.toUpperCase()}`
+})
+
+addChoice.addEventListener('click', () => { 
+  branches.push(choice.value);
+})
+
+removeCandidate.addEventListener('click', () => { 
+  name.value = '';
+  rollNo.value = '';
+  rank.value = '';
+  choice.value = '';
+
+  preview_name.innerHTML = "Name:";
+  preview_rollno.innerHTML = "Roll No:";
+  preview_rank.innerHTML = "Rank:";
+  preview_branches.innerHTML = `Branch:`;
+})
+
 
---
+++ frontend/rankwiseAlgorithm.js
@@ -0,0 +1,32 @@
+import { courses } from "../data/courses.js";
+import { seatMatrix } from "../seatMatrix.js";
+import { candidates } from "../data/candidates.js";
+
+//sorting the students rankwise
+let rankwise = candidates.sort((a, b) => a.rank - b.rank);
+
+for (let i = 0; i < rankwise.length; i++) {
+  let candidate = rankwise[i];
+  let choice = Object.values(candidate.choices);
+
+  //iterating through each choice
+  innerloop: for (let j = 0; j < choice.length; j++) {
+    //iterating through each course that the college provides
+    for (let k = 0; k < courses.length; k++) {
+      //check for seats
+      if (choice[j] === courses[k].name && courses[k].seats !== 0) {
+        // Optionally assign the course to the candidate here
+        seatMatrix.push({
+          name: candidate.name,
+          candidate_rank: candidate.rank,
+          alloted_branch: courses[k].name,
+        });
+
+        courses[k].seats -= 1; //decrement the available seats
+        break innerloop;
+      }
+    }
+  }
+}
+
+console.log(seatMatrix);
